4495a67636d7fb7e58fabacf94fffe49536fb15b
saajraval <saajravals7r@gmail.com>
2022-05-13 12:16:15 +0530

    database connection

--- src/main/java/com/util/DbConnection.java
+++ src/main/java/com/util/DbConnection.java
@@ -1,9 +1,27 @@
 package com.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DbConnection {
 
 	public static void main(String[] args) {
 		
-		
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/advancejavaavengers";
+		String userName = "root";
+		String password = "root";
+
+		try {
+			Class.forName(driver);
+			Connection con = DriverManager.getConnection(url, userName, password);
+			if(con == null ) {
+				System.out.println("Database not connected ...");
+			}else{
+				System.out.println("Database connected ...");
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}//end of try catch 
 	}
 }
